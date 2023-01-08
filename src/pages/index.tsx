35d78126ef5fed7dd31a0f7141e3b6f29f9c8aa4
Erick Pham <erick.pham.dev@gmail.com>
2023-01-08 22:01:15 +0700

    add signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,12 @@ export default function Home() {
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
+        <Link href="/signin" color="secondary">
+          Go to the signIn page
+        </Link>
+        <Link href="/messages" color="secondary">
+          Go to the chat page
+        </Link>
         <ProTip />
         <Copyright />
       </Box>
